271ffde3d067420abca3dfcc3c8c9ef4e86a7ac0
Arne Gevaert <arnegevaert@gmail.com>
2022-03-31 16:15:15 +0200

    compare PermutationSampler to PDPSHAP

--- src/main.py
+++ src/main.py
@@ -8,14 +8,20 @@ from time import time
 from pdp_decomp import PDPShapleySampler
 
 
-def shap_sampling(model, to_explain, X_bg):
-    print("Shapley sampling using SHAP library...")
-    start_t = time()
-    explainer = shap.explainers.Sampling(model, X_bg)
-    values = explainer.shap_values(to_explain)
-    end_t = time()
-    print(f"Done in {end_t - start_t:.2f} seconds")
-    return values
+def get_corrs(values1: np.ndarray, values2: np.ndarray):
+    if values1.shape != values2.shape:
+        raise ValueError(f"Shapes don't match: {values1.shape}, {values2.shape}")
+    pearsons = []
+    spearmans = []
+    for i in range(values1.shape[0]):
+        for j in range(values2.shape[2]):
+            pearson = pearsonr(values1[i, :, j], values2[i, :, j])[0]
+            if not np.isnan(pearson):
+                pearsons.append(pearson)
+            spearman = spearmanr(values1[i, :, j], values2[i, :, j])[0]
+            if not np.isnan(spearman):
+                spearmans.append(spearman)
+    return np.average(pearsons), np.average(spearmans)
 
 
 
@@ -34,13 +40,15 @@ if __name__ == "__main__":
     rng.shuffle(X_bg)
     X_bg = X_bg[:100, :]
 
-    sampling_values = np.array(shap_sampling(knn.predict_proba, X_test[:3, :], X_bg)).transpose((1,2,0))
+    print("Loading sampling values...")
+    with open("data/values.npy", "rb") as fp:
+        sampling_values = np.load(fp)
+    print("Done.")
 
-    """
     print("Computing PDP decomposition...")
     start_t = time()
 
-    explainer = PDPShapleySampler(lambda x: knn.predict_proba(x), X_bg[:100], num_outputs=2, max_dim=2)
+    explainer = PDPShapleySampler(knn.predict_proba, X_bg[:100], num_outputs=2, max_dim=2)
 
     end_t = time()
     print(f"Done in {end_t - start_t:.2f} seconds")
@@ -48,26 +56,24 @@ if __name__ == "__main__":
 
     print("Computing Shapley values via PDP...")
     start_t = time()
-    pdp_values = explainer.estimate_shapley_values(X_test[:3, :])
+    pdp_values = explainer.estimate_shapley_values(X_test)
     end_t = time()
     print(f"Done in {end_t - start_t:.2f} seconds")
 
-    print(pdp_values.shape)
-    """
+    pearson, spearman = get_corrs(pdp_values, sampling_values)
+    print("Correlations:")
+    print(f"\tPearson: {pearson}")
+    print(f"\tSpearman: {spearman}")
     
+    print("Computing Shapley values via PermutationExplainer...")
+    start_t = time()
     med = np.median(X_train, axis=0).reshape((1,X_train.shape[1]))
-
     explainer = shap.Explainer(knn.predict_proba, med)
-    pdp_values = explainer(X_test[:3, :]).values
-
-    print(pdp_values.shape)
-    print(sampling_values.shape)
-
-    pearsons = []
-    spearmans = []
-    for i in range(pdp_values.shape[0]):
-        for j in range(pdp_values.shape[2]):
-            pearsons.append(pearsonr(pdp_values[i, :, j], sampling_values[i, :, j])[0])
-            spearmans.append(spearmanr(pdp_values[i, :, j], sampling_values[i, :, j])[0])
-    print(f"Avg pearson correlation: {np.average(pearsons):.2f}")
-    print(f"Avg spearman correlation: {np.average(spearmans):.2f}")
+    permutation_values = explainer(X_test).values
+    end_t = time()
+    print(f"Done in {end_t - start_t:.2f} seconds")
+    
+    pearson, spearman = get_corrs(permutation_values, sampling_values)
+    print("Correlations:")
+    print(f"\tPearson: {pearson}")
+    print(f"\tSpearman: {spearman}")
