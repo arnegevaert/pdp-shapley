8029ca0935e601146f9486b73e9d6f139fd97eb0
Arne Gevaert <arnegevaert@gmail.com>
2022-06-14 14:00:10 +0200

    fixed shap explainer values shapes

--- src/util/shapley_values.py
+++ src/util/shapley_values.py
@@ -10,10 +10,16 @@ def compute_shapley_values(pred_fn, X_bg, X, explainer: str):
         X = X.to_numpy()
     if explainer == "permutation":
         explainer = PermutationExplainer(pred_fn, X_bg)
-        return explainer(X).values
+        values = explainer(X).values
+        return values if len(values.shape) == 3 else np.expand_dims(values, -1)
     elif explainer == "kernel":
         explainer = KernelExplainer(pred_fn, X_bg)
-        return np.transpose(explainer.shap_values(X), (1, 2, 0))
+        values = explainer.shap_values(X)
+        if len(values.shape) < 3:
+            return np.expand_dims(values, -1)
+        else:
+            return np.transpose(values, (1, 2, 0))
     elif explainer == "sampling":
         explainer = SamplingExplainer(pred_fn, X_bg)
-        return explainer(X).values
+        values = explainer(X).values
+        return values if len(values.shape) == 3 else np.expand_dims(values, -1)
