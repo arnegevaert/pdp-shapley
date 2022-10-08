3f7cd8258f247c96c6827222894872558260b6f2
Arne Gevaert <arne.gevaert@ugent.be>
2022-10-08 11:48:38 +0200

    get_significant_subsets

--- src/pddshap/coe.py
+++ src/pddshap/coe.py
@@ -8,99 +8,55 @@ from tqdm import tqdm
 Model = NewType("Model", Callable[[npt.NDArray], npt.NDArray])
 
 
-def cost_of_exclusion_alt(data: npt.NDArray, model: Model, feature_set: Tuple) -> float:
-    inner_sum = np.zeros(shape=(data.shape[0]))
-    shuffled_data = np.copy(data)
-    np.random.shuffle(shuffled_data)
+class CostOfExclusionEstimator:
+    def __init__(self, data: npt.NDArray, model: Model):
+        self.data = data
+        self.model = model
+        self.shuffled_idx = np.arange(data.shape[0])
+        np.random.shuffle(self.shuffled_idx)
+        self.model_evaluations = {
+            (): self.model(self.data[self.shuffled_idx, :])
+        }
+        self.model_variance = np.var(self.model_evaluations[()])
 
-    for i in range(len(feature_set) + 1):
-        multiplier = 1 if (len(feature_set) - i) % 2 == 0 else -1
-        for subset in combinations(feature_set, i):
-            term_data = np.copy(shuffled_data)
-            term_data[:, subset] = data[:, subset]
-            inner_sum += multiplier * model(term_data)
+    def get_significant_subsets(self, variance_explained=0.9):
+        result = {}
+        cardinality = 0
+        num_columns = self.data.shape[1]
+        # Candidates are all subsets that might be significant, i.e. all subsets for which all subsubsets are significant
+        candidates = [(i,) for i in range(num_columns)]
+        while len(candidates) > 0:
+            cardinality += 1
+            # Compute CoE for each of the subsets, include if significant
+            result[cardinality] = [subset for subset in candidates if self.cost_of_exclusion(subset) > 1 - variance_explained]
+            # For each combination of 2 significant subsets, we test if the cardinality of the union is equal to
+            # the current cardinality + 1. At the end, all subsets with count[subset] == cardinality * (cardinality + 1)
+            # are those subsets for which all subsubsets are significant. These are the candidates for the next iterations.
+            counts = defaultdict(lambda: 0)
+            for set1, set2 in product(result[cardinality], result[cardinality]):
+                union = set(set1).union(set(set2))
+                if len(union) == cardinality + 1:
+                    counts[tuple(union)] += 1
+            candidates = [subset for subset in counts.keys() if counts[subset] == cardinality * (cardinality + 1)]
+        return result
 
-    return np.sum(np.power(inner_sum, 2)) / (data.shape[0] * 2**len(feature_set))
+    def cost_of_exclusion(self, feature_set: Tuple, relative=True):
+        inner_sum = np.zeros(self.data.shape[0])
+        for i in range(len(feature_set) + 1):
+            for subset in combinations(feature_set, i):
+                multiplier = 1 if (len(feature_set) - len(subset)) % 2 == 0 else -1
+                inner_sum += multiplier * self._get_model_evaluation(subset)
+        coe = np.sum(np.power(inner_sum, 2)) / (self.data.shape[0] * 2**len(feature_set))
+        if relative:
+            return coe/self.model_variance
+        return coe
 
-
-
-def cost_of_exclusion(data: npt.NDArray, model: Model, subset: Tuple, num_iterations: int = 1) -> float:
-    """
-    Compute the cost of exclusion for a given subset of features.
-    :param data: background dataset used for the estimation (NDArray[num_rows, num_features])
-    :param model: model to be approximated (Callable[[NDArray], NDArray])
-    :param subset: subset of features to be excluded
-    :param num_iterations: number of iterations
-    :return: cost of exclusion for the given subset and model (float)
-    """
-    columns = list(range(data.shape[1]))
-    shuffled_data_arrays = [np.copy(data) for _ in range(num_iterations)]
-    for arr in shuffled_data_arrays:
-        np.random.shuffle(arr)
-    model_output = model(data)
-    model_variance = np.var(model_output, axis=0)
-
-    invalid_columns = [col for col in subset if col not in columns]
-    if len(invalid_columns) > 0:
-        raise ValueError(f"Invalid columns in subset {subset}: {invalid_columns}")
-
-    # Build the upper bound ANOVA structure U
-    upper_bound_structure = np.array([np.delete(np.arange(data.shape[1]), i) for i in subset])
-    # Use an empirical estimate of the restricted ANOVA model to estimate the model output
-    estimate = np.zeros_like(model_output)
-    for j in range(upper_bound_structure.shape[0]):
-        multiplier = 1 if j % 2 == 0 else -1
-        # Get all j-way intersections of rows in upper_bound_structure
-        # "j-way intersection" is defined as an intersection between j+1 elements
-        # Specifically, "0-way intersections" are just elements
-        index_subsets = list(combinations(range(upper_bound_structure.shape[0]), j+1))
-        for index_subset in index_subsets:
-            # Build the intersection from row indices
-            intersection = upper_bound_structure[index_subset[0], :]
-            for k in range(1, len(index_subset)):
-                intersection = np.intersect1d(intersection, upper_bound_structure[index_subset[k], :], assume_unique=True)
-            # Replace the corresponding columns of shuffled data with the original data,
-            # get output of the model on this partially shuffled data
-            intersection_estimate = np.zeros_like(estimate)
-            for shuffled_data in shuffled_data_arrays:
-                cur_shuffled_data = np.copy(shuffled_data)
-                cur_shuffled_data[:, intersection] = data[:, intersection]
-                intersection_estimate += multiplier * model(cur_shuffled_data) / num_iterations
-            estimate += intersection_estimate
-    coe = np.sum(np.power(model_output - estimate, 2)) / (2**len(subset) * data.shape[0])
-    return coe
-
-
-def compute_significant_subsets(data: npt.NDArray, model: Model, threshold: float = 0.01,
-                                num_iterations: int = 1) -> Dict[int, List[Tuple]]:
-    """
-    Compute all significant subsets of features according to the Cost of Exclusion.
-    :param data: Background dataset used for computing CoE (NDArray[num_rows, num_features])
-    :param model: Model to be approximated (Callable[[NDArray], NDArray])
-    :param threshold: CoE threshold to include a given subset
-    :param num_iterations: Number of iterations in computation of CoE
-    :return: Dictionary of subsets with CoE>threshold. Each entry of the dictionary contains a list of subsets of
-        a given cardinality. Example: result[3] = [significant subsets of length 3]
-    """
-    result = {}
-    cardinality = 0
-    num_columns = data.shape[1]
-    # Candidates are all subsets that might be significant, i.e. all subsets for which all subsubsets are significant
-    candidates = [(i,) for i in range(num_columns)]
-    pbar = tqdm()
-    while len(candidates) > 0:
-        cardinality += 1
-        # Compute CoE for each of the subsets, include if significant
-        result[cardinality] = [subset for subset in candidates
-                               if cost_of_exclusion(data, model, subset, num_iterations) > threshold]
-        # For each combination of 2 significant subsets, we test if the cardinality of the union is equal to
-        # the current cardinality + 1. At the end, all subsets with count[subset] == cardinality * (cardinality + 1)
-        # are those subsets for which all subsubsets are significant. These are the candidates for the next iterations.
-        counts = defaultdict(lambda: 0)
-        for set1, set2 in tqdm(product(result[cardinality], result[cardinality])):
-            union = set(set1).union(set(set2))
-            if len(union) == cardinality + 1:
-                counts[tuple(union)] += 1
-        candidates = [subset for subset in counts.keys() if counts[subset] == cardinality * (cardinality + 1)]
-        pbar.update(1)
-    return result
+    def _get_model_evaluation(self, feature_set: Tuple):
+        if feature_set in self.model_evaluations.keys():
+            return self.model_evaluations[feature_set]
+        else:
+            data = self.data[self.shuffled_idx, :]
+            data[:, feature_set] = self.data[:, feature_set]
+            result = self.model(data)
+            self.model_evaluations[feature_set] = result
+            return result
--- src/sandbox/coe_test.py
+++ src/sandbox/coe_test.py
@@ -1,4 +1,4 @@
-from pddshap.coe import compute_significant_subsets, cost_of_exclusion, cost_of_exclusion_alt
+from pddshap.coe import CostOfExclusionEstimator
 from synth.random_multilinear_polynomial import RandomMultilinearPolynomial, MultilinearPolynomial
 import numpy as np
 from itertools import chain, combinations
@@ -14,47 +14,14 @@ if __name__ == "__main__":
     num_features = 10
     mp = RandomMultilinearPolynomial(num_features, [-1, 10, 5, 1], coefficient_generator=lambda: 10 + np.random.random())
     """
-    num_features = 6
-    num_iterations = 100
+    num_features = 5
+    num_iterations = 1
     mp = MultilinearPolynomial(num_features, coefficients={
-        (): 10,
-        (1,2,3): 5.
+        (1,2,3): 5.,
+        (0,1,2,3,4): 1.
     })
-    data = np.random.normal(size=(1000, num_features))
-    print(np.var(mp(data)))
+    data = np.random.normal(size=(100000, num_features))
+    coe_est = CostOfExclusionEstimator(data, mp)
 
-    for key in [(0,), (4,), (0,4), (0,4,5), (1,), (1,2), (1,2,3)]:
-    #for key in [(0,4,5)]:
-        print(key)
-        coes = [cost_of_exclusion_alt(data, mp, key) for _ in range(num_iterations)]
-        print(f"\t{np.average(coes):.3f}")
-        print()
-
-    """
-    significant_subsets = compute_significant_subsets(data, mp, num_iterations=1, threshold=1e-10)
-
-    coefficients = mp.coefficients.keys()
-    true_subsets = []
-    for coef in coefficients:
-        subsets = list(powerset(coef))
-        for subset in subsets:
-            if tuple(sorted(subset)) not in true_subsets:
-                true_subsets.append(tuple(sorted(subset)))
-
-    tp, fp, fn = 0, 0, 0
-    significant_subsets_flattened = []
-    for key in significant_subsets:
-        significant_subsets_flattened += significant_subsets[key]
-
-    for sig_subset in significant_subsets_flattened:
-        if sig_subset in true_subsets:
-            tp += 1
-        else:
-            fp += 1
-    for subset in true_subsets:
-        if subset not in significant_subsets_flattened and len(subset) > 0:
-            fn += 1
-
-    print(f"Precision: {tp / (tp + fp):.2f}")
-    print(f"Recall: {tp / (tp + fn):.2f}")
-    """
+    significant_subsets = coe_est.get_significant_subsets(variance_explained=0.9)
+    print(significant_subsets)
